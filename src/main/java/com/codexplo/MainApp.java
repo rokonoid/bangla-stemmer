2ae1c1b135c411e9b34361d3de4c124803097c2c
Bazlur Rahman Rokon <anm_brr@yahoo.com>
2014-04-19 03:14:08 +0600

    Say helloworld in chinese 100 times

--- src/main/java/com/codexplo/MainApp.java
+++ src/main/java/com/codexplo/MainApp.java
@@ -6,5 +6,9 @@ package com.codexplo;
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("你好世界");
+        }
     }
 }
