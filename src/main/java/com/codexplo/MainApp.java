45b48e3c1d43beebdcd782ac5885d0cb7a45da10
Bazlur Rahman Rokon <anm_brr@yahoo.com>
2014-04-19 02:58:11 +0600

    Remove ignored files

---
+++ src/main/java/com/codexplo/MainApp.java
@@ -0,0 +1,10 @@
+package com.codexplo;
+
+/**
+ * Created by rokonoid on 4/19/14.
+ */
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
