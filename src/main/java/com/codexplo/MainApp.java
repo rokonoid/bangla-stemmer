298bbb58e21b797c8e9c3eaf8c13a13fb9ab99b9
Bazlur Rahman Rokon <anm_brr@yahoo.com>
2014-04-19 03:08:15 +0600

    Say 100 times in english

--- src/main/java/com/codexplo/MainApp.java
+++ src/main/java/com/codexplo/MainApp.java
@@ -5,6 +5,9 @@ package com.codexplo;
  */
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        //lets say 100 times 
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Hello world");
+        }
     }
 }
